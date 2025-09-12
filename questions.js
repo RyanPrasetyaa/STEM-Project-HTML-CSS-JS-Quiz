f3e19af31de38d46fde26b55214d5a69d3d2e405
Seanstanislaus <134819954+Seanstanislaus@users.noreply.github.com>
2025-09-13 06:26:11 +0700

    menit 1.01.30

---
+++ questions.js
@@ -0,0 +1,42 @@
+let questions = [
+    { 
+        numb : 1,
+        question : "What does HTML stands for?",
+        answer : "C. Hyper Text Markup Language",
+        options : [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+
+
+        ]
+    },
+    { 
+        numb : 2,
+        question : "dasadsfaada",
+        answer :  "C. ",
+        options : [
+            "A. ",
+            "B. ",
+            "C. ",
+            "D. "
+        ]
+    },
+    { 
+        numb : 3,
+        question : "",
+        answer : "",
+        options : [
+
+        ]
+    },
+    { 
+        numb : 4,
+        question : "",
+        answer : "",
+        options : [
+
+        ]
+    },
+]
--- script.js
+++ script.js
@@ -1,22 +1,84 @@
-const start8tn = document.querySelector(".start-btn");
+const startBtn = document.querySelector(".start-btn");
 const popupinfo = document.querySelector(".popup-info");
-const exit8tn = document.querySelector(".exit-btn");
+const exitBtn = document.querySelector(".exit-btn");
 const main = document.querySelector(".main");
-const continue8tn = document.querySelector(".continue-btn");
-const quisSection = document.querySelector(".quis-section");
+const continueBtn = document.querySelector(".continue-btn");
+const quizSection = document.querySelector(".quiz-section");
+const quisBox = document.querySelector(".quis-box");
 
-start8tn.onclick = () => {
+
+startBtn.onclick = () => {
   popupinfo.classList.add("active");
   main.classList.add("active");
 };
 
-exit8tn.onclick = () => {
+exitBtn.onclick = () => {
   popupinfo.classList.remove("active");
   main.classList.remove("active");
 };
 
-continue8tn.onclick = () => {
-  quisSection.classList.add("active");
+continueBtn.onclick = () => {
+  quizSection.classList.add("active");
   popupinfo.classList.remove("active");
   main.classList.remove("active");
+  quisBox.classList.add("active");
+
+  showQuestions(0);
 };
+
+let questionCount = 0;
+let questionNumb = 1;
+
+const nextBtn = document.querySelector('.next-btn');
+
+nextBtn.onclick = () => {
+  if (questionCount < questions.length - 1) {
+      questionCount++;
+      showQuestions(questionCount);
+  }
+  else {
+    console.log('Question Completed');
+  }
+}
+
+const optionList = document.querySelector('.option-list');
+
+function showQuestions(index) {
+  const questionText = document.querySelector('.question-text');
+  questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+  let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+      <div class="option"><span>${questions[index].options[1]}</span></div>
+      <div class="option"><span>${questions[index].options[2]}</span></div>
+      <div class="option"><span>${questions[index].options[3]}</span></div>`
+
+      optionList.innerHTML  = optionTag;
+
+      const option = document.querySelectorAll('.option');
+      for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute('onclick', 'optionSelected(this)');
+   }
+}
+
+function optionSelected(answer) {
+  let userAnswer = answer.textContent;
+  let correctAnswer = questions[questionCount].answer;
+  let allOptions = optionList.children.length;
+
+  if (userAnswer == correctAnswer) {
+    answer.classList.add('correct');
+  }
+  else {
+   answer.classList.add('incorrect');
+  }
+
+  for (let i = 0; i < allOptions; i++) {
+    optionList.children[i].classList.add('disabled');
+  }
+}
+  
+
+function questionCounter(index) {
+  const questionTotal = document.querySelector('.question-total');
+  questionTotal.textContent = `${index} of ${questions.length}  Questions`;
+}

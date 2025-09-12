8216e0d0af9c4e1a4f9cd0169d0240d0e18bd3e8
Seanstanislaus <134819954+Seanstanislaus@users.noreply.github.com>
2025-09-12 10:46:55 +0700

    23.53

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const start8tn = document.querySelector(".start-btn");
 const popupinfo = document.querySelector(".popup-info");
+const exit8tn = document.querySelector(".exit-btn");
+const main = document.querySelector(".main");
+const continue8tn = document.querySelector(".continue-btn");
+const quisSection = document.querySelector(".quis-section");
 
 start8tn.onclick = () => {
   popupinfo.classList.add("active");
+  main.classList.add("active");
+};
+
+exit8tn.onclick = () => {
+  popupinfo.classList.remove("active");
+  main.classList.remove("active");
+};
+
+continue8tn.onclick = () => {
+  quisSection.classList.add("active");
+  popupinfo.classList.remove("active");
+  main.classList.remove("active");
 };
